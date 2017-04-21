c3cd160e0e2b6711f80509a9f4bd2efb6bfef29e
Jonathan Ferreira <joohncruzrocha@gmail.com>
2017-04-21 16:29:45 -0300

    Adicionando views e trabalhando com reduce e map

--- modulo_um/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ modulo_um/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,12 +8,16 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event) {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
 
     }
@@ -29,8 +33,8 @@ class NegociacaoController {
     _limpaFormulario() {
 
         this._inputData.value = '';
-        this._inputQuantidade.value = 1;
-        this._inputValor = 0.0;
+        this._inputQuantidade.value = '';
+        this._inputValor.value = '';
         this._inputData.focus();
 
     }
